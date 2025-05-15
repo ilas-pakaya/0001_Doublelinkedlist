5f8d177d40f0c1f47653dfaf6c537837f0d04927
ilas-pakaya <ilaspakaya@ilas-2.local>
2025-05-15 10:34:56 +0700

    menambahkan node

--- DoubleLinkList.cpp
+++ DoubleLinkList.cpp
@@ -8,3 +8,21 @@ class mode {
         node *next;
         node *prev;
 };
+
+class doubleLinkList{
+    private:
+        node *START;
+
+    public:
+        doubleLinkList(){
+            START = NULL;
+        }
+        void addNode(){
+            int nim;
+            string nama;
+            cout << "\nEnter the roll number of the student";
+            cin >> nim;
+            node *newNode = new node();
+            newNode->nomhs = nim;
+        }
+}
