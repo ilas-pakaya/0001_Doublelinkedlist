781bf838aa9f93b59417238a6067911fa226ccbc
ilas-pakaya <ilaspakaya@ilas-2.local>
2025-05-15 11:01:30 +0700

    insert last node

--- DoubleLinkList.cpp
+++ DoubleLinkList.cpp
@@ -51,5 +51,8 @@ class doubleLinkList{
 
             newNode -> next = current -> next;
             newNode -> prev = current;
+            if (current -> next != NULL)
+                current -> next -> prev = newNode;
+            current -> next = newNode;
         }
 };
